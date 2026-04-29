1522a8f3414faa56490b42381a01a89ff677f811
Zahra Aulia <13224007@mahasiswa.itb.ac.id>
2026-04-29 10:45:04 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -22,8 +22,8 @@ int proses (const void *x, const void *y){
     Artefak *a2 = (Artefak *) y;
     int cmpkat = strcmp(a1->kategori, a2->kategori);
     if (cmpkat != 0) return cmpkat;
-    if (a1->tahun != a2->tahun) return (a2->tahun - a1->tahun); 
-    if (a1->nilai != a2->nilai) return (a1->nilai - a2->nilai);
+    if (a1->tahun != a2->tahun) return (a1->tahun - a2->tahun); 
+    if (a1->nilai != a2->nilai) return (a2->nilai - a1->nilai);
     return strcmp(a1->nama, a2->nama);
 }
 
@@ -32,8 +32,7 @@ int main() {
     scanf("%d",&n);
 
     Artefak artefak[n];
-
-    int i;
+ 
     for (i = 0; i < n; i++) {
         scanf("%s %s %d %d", artefak[i].kategori, artefak[i].nama, &artefak[i].tahun, &artefak[i].nilai);
     }
